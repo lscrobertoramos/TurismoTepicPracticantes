2f066d98105598fdee6b568c94b5c3de31f96781
lscrobertoramos <roberto.ramos.nay@gmail.com>
2018-01-22 16:05:06 -0700

    Se agrego un texto para la prueba

--- TurismoTepicCi/app/src/main/java/com/ut3/ehg/turismotepic/MapsActivity.java
+++ TurismoTepicCi/app/src/main/java/com/ut3/ehg/turismotepic/MapsActivity.java
@@ -111,7 +111,7 @@ public class MapsActivity extends Fragment implements LocationListener, OnMapRea
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        System.out.print("");
+        System.out.print("Cambio 2");
 
 
         return root;
